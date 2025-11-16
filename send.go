aa53e52bb447e95dd736be45c71f73a2ccb53885
Guilherme Jansen <guilhermejansenoficial@gmail.com>
2025-11-16 03:19:09 -0300

    merge: ajust merge main

--- send.go
+++ send.go
@@ -972,6 +972,13 @@ func getButtonTypeFromMessage(msg *waE2E.Message) string {
 		return "list_response"
 	case msg.InteractiveResponseMessage != nil:
 		return "interactive_response"
+	case msg.InteractiveMessage != nil:
+		carouselMsg := msg.InteractiveMessage.GetCarouselMessage()
+		if carouselMsg != nil && carouselMsg.Cards != nil {
+			return "interactive"
+		}
+
+		return "interactive"
 	default:
 		return ""
 	}
@@ -990,10 +997,66 @@ func getButtonAttributes(msg *waE2E.Message) waBinary.Attrs {
 	case msg.ListMessage != nil:
 		return waBinary.Attrs{
 			"v":    "2",
-			"type": strings.ToLower(waE2E.ListMessage_ListType_name[int32(msg.ListMessage.GetListType())]),
+			"type": strings.ToLower(waE2E.ListMessage_ListType_name[int32(*waE2E.ListMessage_PRODUCT_LIST.Enum())]),
+		}
+	case msg.InteractiveMessage != nil:
+		isPaymentInfoButton := false
+		for _, button := range msg.InteractiveMessage.GetNativeFlowMessage().GetButtons() {
+			if button.GetName() == "payment_info" {
+				isPaymentInfoButton = true
+				break
+			}
+		}
+		if isPaymentInfoButton {
+			return waBinary.Attrs{
+				"v":    "1",
+				"type": "native_flow",
+			}
+		}
+		carouselMsg := msg.InteractiveMessage.GetCarouselMessage()
+		if carouselMsg != nil && carouselMsg.Cards != nil {
+			return waBinary.Attrs{
+				"type": "native_flow",
+			}
+		} else {
+			return waBinary.Attrs{
+				"type": "native_flow",
+			}
 		}
 	default:
 		return waBinary.Attrs{}
+	}
+}
+
+func getButtonContent(msg *waE2E.Message) []waBinary.Node {
+	switch {
+	case msg.InteractiveMessage != nil:
+		buttons := msg.InteractiveMessage.GetNativeFlowMessage().GetButtons()
+		isPaymentInfoButton := false
+		for _, button := range buttons {
+			if button.GetName() == "payment_info" {
+				isPaymentInfoButton = true
+				break
+			}
+		}
+		if isPaymentInfoButton {
+			return []waBinary.Node{{
+				Tag: "native_flow",
+				Attrs: waBinary.Attrs{
+					"name": "payment_info",
+				},
+			}}
+		} else {
+			return []waBinary.Node{{
+				Tag: "native_flow",
+				Attrs: waBinary.Attrs{
+					"v":    "2",
+					"name": "mixed",
+				},
+			}}
+		}
+	default:
+		return nil
 	}
 }
 
@@ -1111,14 +1174,26 @@ func (cli *Client) getMessageContent(
 	}
 
 	if buttonType := getButtonTypeFromMessage(message); buttonType != "" {
-		content = append(content, waBinary.Node{
-			Tag: "biz",
-			Content: []waBinary.Node{{
-				Tag:   buttonType,
-				Attrs: getButtonAttributes(message),
-			}},
-		})
+		if buttonType == "list" {
+			content = append(content, waBinary.Node{
+				Tag: "biz",
+				Content: []waBinary.Node{{
+					Tag:   buttonType,
+					Attrs: getButtonAttributes(message),
+				}},
+			})
+		} else {
+			content = append(content, waBinary.Node{
+				Tag: "biz",
+				Content: []waBinary.Node{{
+					Tag:     buttonType,
+					Attrs:   getButtonAttributes(message),
+					Content: getButtonContent(message),
+				}},
+			})
+		}
 	}
+
 	return content
 }
 
